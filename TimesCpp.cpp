62bbad086294213a7d32b156ec22560ce25d8b39
Alfian <viansmada100@gmail.com>
2020-10-16 13:24:56 +0700

    Please merge mine, I need to complete the hacktober... Thanks

---
+++ TimesCpp.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include <cstdlib>
+using namespace std;
+/*Fungsi Integer */
+int perkalian(int a, int b){
+int hasil;
+int jumlah;
+hasil = a * b;
+jumlah = a + b;
+}
+main(){
+int x,y;
+cout<<"Masukkan nilai x = ";
+cin>>x;
+cout<<"Masukkan nilai y = ";
+cin>>y;
+
+cout<<"Hasil perkalian adalah "<<perkalian(x,y)<<endl;
+system("PAUSE");
+}
